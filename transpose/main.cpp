6dcef71c730ba847a792569f6176b715616b8043
Oleksandr Ishchuk <o.v.ishchuk@gmail.com>
2018-12-11 13:54:45 +0200

    almost no changes. algorithm in head

--- transpose/main.cpp
+++ transpose/main.cpp
@@ -1,6 +1,16 @@
-
+#include <iostream>
 
 int ** transpose(const int * const * m, unsigned rows, unsigned cols)
 {
-  /* ... */
+  int * transM 
+  for (int i = 0; i < rows; ++i)
+  {
+    for
+  }
+  return m;
+}
+
+int main(void)
+{
+  return 0;
 }
