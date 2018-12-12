e451a4676a5e7f5b3805d8dd0bb8103d46d50853
Oleksandr Ishchuk <o.v.ishchuk@gmail.com>
2018-12-12 21:54:31 +0200

    transM array in traspose

--- transpose/main.cpp
+++ transpose/main.cpp
@@ -2,15 +2,16 @@
 
 int ** transpose(const int * const * m, unsigned rows, unsigned cols)
 {
-  int * transM 
-  for (int i = 0; i < rows; ++i)
-  {
-    for
-  }
-  return m;
+    int ** transM = new int * [rows];
+    transM[0] = new int [rows * cols];
+    for (int index = 1; index != rows; ++index)
+    {
+        transM[index] = transM[index - 1] + cols;
+    }
+    return transM;
 }
 
 int main(void)
 {
-  return 0;
+    return 0;
 }
