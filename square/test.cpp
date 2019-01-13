5f4f71c8943dad99ef728665780e057720121df2
Oleksandr Ishchuk <o.v.ishchuk@gmail.com>
2019-01-13 20:02:57 +0200

    task 3.7-2 done

--- square/test.cpp
+++
@@ -1,5 +0,0 @@
-#include "test.h"
-
-int Test::square(int i) {
-	return i *= i;
-}
--- square/test.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-class Test {
-	public:
-		int square(int);
-};
-
-#endif // TEST_H
